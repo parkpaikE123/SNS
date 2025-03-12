b528a8e7976d47497b677206d2e20a2cd104e677
wjdals2285 <wjdals2285@naver.com>
2025-03-12 15:44:58 +0900

    [류정민] - SNS 프로젝트 추가 - DB 비밀번호 암호화

---
+++ src/main/java/com/ryujm/sns/PersonalSnsApplication.java
@@ -0,0 +1,13 @@
+package com.ryujm.sns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalSnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalSnsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ryujm/sns/PersonalSnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryujm.sns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalSnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
