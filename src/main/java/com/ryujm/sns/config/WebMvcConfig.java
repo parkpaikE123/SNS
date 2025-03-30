941feaa23385a6ce3ed90f1372ff5d7905287777
wjdals2285 <wjdals2285@naver.com>
2025-03-31 01:28:54 +0900

    [류정민] {SNS프로젝트} - session로그인 별 접근 제한 설정

--- src/main/java/com/ryujm/sns/config/WebMvcConfig.java
+++ src/main/java/com/ryujm/sns/config/WebMvcConfig.java
@@ -1,9 +1,11 @@
 package com.ryujm.sns.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.ryujm.sns.interceptor.PermissonInterceptor;
 import com.ryujm.sns.common.FileManager;
 
 @Configuration
@@ -13,6 +15,14 @@ public class WebMvcConfig implements WebMvcConfigurer{
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/images/**")
 		.addResourceLocations("file:///" + FileManager.FILE_UPLOAD_PATH + "/");
+	}
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new PermissonInterceptor())
+		.addPathPatterns("/**")
+		.excludePathPatterns("/user/logout"
+							, "/static/**"
+							, "/images/**");
 	}
 	
 }
